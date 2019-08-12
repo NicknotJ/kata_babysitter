3360f270e85f1b5be62d475c76a56d25492a180d
nicknotj <nicknotjustin@gmail.com>
2019-08-12 14:29:49 -0500

    Added test to UserFamilyInput: Should return Y if letter & not a/b/c.

--- test/test.js
+++ test/test.js
@@ -26,8 +26,10 @@ describe('UserFamilyInput', () => {
   //please note: letter in this case refers only to the 26 letters of the English alphabet
   it('Should return Z if the input is not a letter', () => {
     assert.equal(calculations.userSitFamily(3), 'Z');
-    assert.equal(calculations.userSitFamily('D'), 'D');
-    assert.equal(calculations.userSitFamily('/'), 'Z');
+    assert.equal(calculations.userSitFamily('&'), 'Z');
     assert.equal(calculations.userSitFamily('a'), 'a');
+  });
+  it('Should return Y if the input is not A/a, B/b, or C/c', () => {
+    assert.equal(calculations.userSitFamily('D'), 'Y');
   });
 });
