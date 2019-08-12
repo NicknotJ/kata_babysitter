c869a39fd54ff42974588fb89ad140eb7b49ca9d
nicknotj <nicknotjustin@gmail.com>
2019-08-12 13:41:14 -0500

    Added second test for positive numbers greater than 1 for userSitHours)

--- test/test.js
+++ test/test.js
@@ -5,5 +5,9 @@ const assert = chai.assert;
 describe('UserInput', () => {
   it('Should return the inputted number', () => {
     assert.equal(calculations.userSitHours(1), 1);
+  });
+  it('Should return any positive number', () => {
+    assert.equal(calculations.userSitHours(5), 5);
+    assert.equal(calculations.userSitHours(2), 2);
   })
 })
