4fde389356943fc02d30d0ca7060ebfc91801a2a
nicknotj <nicknotjustin@gmail.com>
2019-08-13 10:27:58 -0500

    userSitHours does not allow babysit times before 5 pm.  Naive approach. tests passing

--- calculations.js
+++ calculations.js
@@ -1,6 +1,14 @@
 //Input: Start time, End time
 //Checks: Is start time before 5 pm? Is end time after 4 am? Is end time before or equal to start time?
 function userSitHours(startNumber, startTime, endNumber, endTime){
+  //edge case for 12 pm (12 > 5, but earlier)
+  if(startTime === 'pm' && startNumber === 12){
+    return -1;
+  }
+  //cannot babysit before 5pm
+  if(startTime === 'pm' && startNumber < 5){
+    return -1;
+  }
   if(startTime !== endTime){
     endNumber += 12;
   }
