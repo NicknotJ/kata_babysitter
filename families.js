5f47a192236c7b6ebf4bf7ac42b8be4554947191
nicknotj <nicknotjustin@gmail.com>
2019-08-14 21:40:41 -0500

    class Family now accepts an object for timeSpan. Just exists at the moment.

--- families.js
+++ families.js
@@ -1,14 +1,15 @@
 class Family {
-  constructor(name){
+  constructor(name, timeSpan){
     this.name = name;
+    this.timeSpan = timeSpan;
   }
 
   calculatePay(hours, rate){
     return hours * rate;
   }
 }
-
-const familyA = new Family('A');
+//startNumber, startTime, endNumber, endTime
+const familyA = new Family('A', [{startNumber: 5, startTime: 'pm', endNumber: '10', endTime: 'pm'}, {startNumber: 10, startTime: 'pm', endNumber: '4', endTime: 'am'}]);
 //Family A pays $15 per hour before 11pm, and $20 per hour the rest of the night
 
 
