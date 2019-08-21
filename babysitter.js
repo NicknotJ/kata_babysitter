7021be1ba248f6ab5b6b095050906bdd8028d47c
nicknotj <nicknotjustin@gmail.com>
2019-08-21 16:29:56 -0500

    Wrote a flowchart of the human to CLI interaction.

--- babysitter.js
+++ babysitter.js
@@ -6,12 +6,22 @@ const readline = require('readline');
 function main(){
   const r1 = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
   });
-  r1.question('Which family did you sit for?', (answer) => {
-    console.log(`You sat for family${answer}`);
-    r1.close();
-  });
+  //Flowchart of user actions/interactions
+  //Generic Greeting: Welcome to Baby$at V 0.01
+  //Question: Which family did you sit for?
+    //if answer is Aa/Bb/Cc -> store variable, continue
+    //if answer returns Y -> Sorry, no family'letter' in database
+    //if answer returns Z -> Please input a roman letter
+  //When did you start sitting (number only?)
+  //When did you start sitting (pm/am)?
+    //check if answer is possible, if not go back to when did you start sitting?
+  //When did you stop sitting (number only?)
+  //When did you stop sitting (pm/am)?
+    //check if answer is possible, if not go back to when did you stop sitting?
+  //Return amount earned
+  //Thank you and have fun spending your hard earned money!
 }
 
 main();
