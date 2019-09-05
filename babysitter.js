ddf0a915d3bd2ecfd4b93fb77124e978a1e0d196
nicknotj <nicknotjustin@gmail.com>
2019-09-05 15:48:31 -0500

    Incredibly naive solution regarding inputting values. Currently does not return the correct amount (returns -1 signifying an error) for totalAmount.

--- babysitter.js
+++ babysitter.js
@@ -3,21 +3,37 @@ const families = require('./families');
 const readline = require('readline');
 
 
-let store = {
-  family: undefined,
+const store = {
+  family: 'Y',
   startNumber: undefined,
   startTime: undefined,
   endNumber: undefined,
   endTime: undefined
 };
 
-function babysit(){
-   console.log('What is your name?');
-   process.stdin.once('data', (chunk) => {
-     let name = chunk.toString().trim();
-     console.log('Hello, ' + name + '!');
-     process.exit();
-   });
+const readlineInterface = readline.createInterface(process.stdin, process.stdout);
+function ask(questionText) {
+  return new Promise((resolve, reject) => {
+    readlineInterface.question(questionText, resolve);
+  });
+}
+async function babysit(){
+   console.log('Hello! Welcome to Baby$at v 0.1.');
+  //  let greetingTimer = (exampleFunction) => setTimeout(exampleFunction, 1500);
+   
+  //  greetingTimer(exampleFunction);
+  store.family = await ask('What family did you sit for?');
+  console.log(store.family);
+  store.startNumber = await ask('What time (number only) did you start sitting?');
+  store.startTime = await ask('Did you start in the am or pm?');
+  store.endNumber = await ask('What time (number only) did you stop sitting?');
+  store.endTime = await ask('Did you stop in the am or pm?');
+  amountEarned = families.families[store.family].totalAmount(store.startNumber, store.startTime, store.endNumber, store.endTime);
+  console.log('Congratulations, you earned ' + amountEarned + 'dollars!');
+}
+
+
+
   //prompt is showing up way later than question
   //Flowchart of user actions/interactions
   //Generic Greeting: Welcome to Baby$at V 0.01
@@ -33,11 +49,6 @@ function babysit(){
     //check if answer is possible, if not go back to when did you stop sitting?
   //Return amount earned
   //Thank you and have fun spending your hard earned money!
-  function end(){
-    r1.close();
-  }
-};
-
 babysit();
 
 
