790387e9aa3a3c3be7ec3eea6353041c1d80b36b
nicknotj <nicknotjustin@gmail.com>
2019-09-07 23:58:36 -0500

    removed unnecessary comments from babysitter.js.

--- babysitter.js
+++ babysitter.js
@@ -43,21 +43,6 @@ async function babysit(){
 
 
 
-  //prompt is showing up way later than question
-  //Flowchart of user actions/interactions
-  //Generic Greeting: Welcome to Baby$at V 0.01
-  //Question: Which family did you sit for (Please input a letter)?
-    //if answer is Aa/Bb/Cc -> store variable, continue
-    //if answer returns Y -> Sorry, no family'letter' in database
-    //if answer returns Z -> Please input a roman letter
-  //When did you start sitting (number only?)
-  //When did you start sitting (pm/am)?
-    //check if answer is possible, if not go back to when did you start sitting?
-  //When did you stop sitting (number only?)
-  //When did you stop sitting (pm/am)?
-    //check if answer is possible, if not go back to when did you stop sitting?
-  //Return amount earned
-  //Thank you and have fun spending your hard earned money!
 babysit();
 
 
