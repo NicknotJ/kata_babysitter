b9bbe805d3e561b1b093434f87059766b01b6f34
nicknotj <nicknotjustin@gmail.com>
2019-09-12 11:28:40 -0500

    Added a new test to babysitter.js which tests if the store.family value updates correctly on user input of A. currently fails.

--- test/babysitter.js
+++ test/babysitter.js
@@ -3,6 +3,8 @@ const families = require('../families');
 const babysitter = require('../babysitter');
 const chai = require('chai');
 const assert = chai.assert;
+//mock-stdin will allow testing of user inputs
+const stdin = require('mock-stdin').stdin();
 
 // describe('BabySat CL', () => {
 //   it('Should exist', () => {
@@ -14,7 +16,10 @@ describe('babySit', () => {
   it('Should exist', () => {
     assert.isDefined(babysitter.babysit, 'babysit is defined');
   });
-  it('Should question the user about what family they sat for and store response', () => {
-    
-  })
+  it('It should update the store.family value with the user input if (A, B, or C)', () => {
+    process.nextTick(function mockResponse() {
+      stdin.send('A');
+    });
+    assert.equal(babysitter.store.family, 'A');
+  });
 })
